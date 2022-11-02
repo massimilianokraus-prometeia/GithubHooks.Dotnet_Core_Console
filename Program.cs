b0de7ebff6298eb5ef0851cdaf5650db7166f6ff
massimilianokraus-prometeia <103130893+massimilianokraus-prometeia@users.noreply.github.com>
2022-11-02 12:00:06 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,1 @@
-﻿Console.WriteLine($"- Commit Hash: {args[0]} \n - Author: {args[1]} \n - Message: {args[2]}");
+﻿Console.WriteLine($".NET Core \n- Commit Hash: {args[0]} \n - Author: {args[1]} \n - Message: {args[2]}");
