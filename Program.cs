3dbd8d9edf23bdb992b3f3177aeb003159920567
massimilianokraus-prometeia <103130893+massimilianokraus-prometeia@users.noreply.github.com>
2022-11-03 11:56:19 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
 ﻿Console.WriteLine($".NET Core \n- Commit Hash: {args[0]} \n - Author: {args[1]} \n - Message: {args[2]}");
+Console.WriteLine(GetJiraIssue(args[2]));
+
+
+static string GetJiraIssue(string message) {
+  return "";
+}
