a6b8112c8af459ad06b141f7eb649017cc3263ec
massimilianokraus-prometeia <massimiliano.kraus@prometeia.com>
2022-11-04 09:53:15 +0200

    commit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
-﻿Console.WriteLine($".NET Core \n- Commit Hash: {args[0]} \n - Author: {args[1]} \n - Message: {args[2]}");
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine($".NET Core \n- Commit Hash: {args[0]} \n - Author: {args[1]} \n - Message: {args[2]}");
 Console.WriteLine(GetJiraIssue(args[2]));
 
 
 static string GetJiraIssue(string message) {
-  return "";
+    var regex = new Regex(@"[A-Z]{2,}-\d+");
+    var match = regex.Match(message);
+    if (match.Success) {
+        return match.Value;
+    } else {
+        return "";
+    }
 }
