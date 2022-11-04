72c1252bf22cd01ee51baa1708ef6018d9777b6b
massimilianokraus-prometeia <103130893+massimilianokraus-prometeia@users.noreply.github.com>
2022-11-04 10:19:56 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿using System.Text.RegularExpressions;
 
 Console.WriteLine($".NET Core App Started \n - Pull Request Title: {args[0]} \n - Pull Request Body: {args[1]}");
-Console.WriteLine(GetJiraIssue(args[0]));
-
+// Console.WriteLine(GetJiraIssue(args[0]));
+return GetJiraIssue(args[0]);
 
 static string GetJiraIssue(string message) {
     var regex = new Regex(@"[A-Z]{2,}-\d+");
